9be58c5ef43809e2e5bd3ef0189fae6c13ee86fe
MarcoGds <marco_guedes@outlook.com>
2022-02-16 21:07:21 -0300

    1.6

--- app/app.js
+++ app/app.js
@@ -1,1 +1,19 @@
 $('#myModal').modal({backdrop:'static, keyboard: false'})
+
+// $(window).on('resize', function() {
+//     var win = $(this);
+//     if (win.width() < 667) {
+//         $('central').removeClass('col-12');
+//     } else {
+//         $('central').addClass('col-12');
+//     }
+// })
+
+// $(window).on('resize', function() {
+//     var win = $(this);
+//     if (win.width() < 667) {
+//         $('lateral').addClass('col-1');
+//     } else {
+//         $('lateral').removeClass('col-1');
+//     }
+// });
